610f00c0884c4c2260587285f65c0b1a0dfc0fdc
Blaxar Waldarax <blaxar.waldarax@gmail.com>
2019-10-20 20:24:26 +0200

    Scraping some API idea

--- dankcaster/src/lib.rs
+++ dankcaster/src/lib.rs
@@ -1,3 +1,26 @@
-pub fn hello_world() {
-    println!("Hello world!");
+extern crate gstreamer as gst;
+use gst::prelude::*;
+
+struct Source {
+    element: gst::Element,
+}
+
+struct WrappedSource<'a> {
+    source: &'a gst::Element,
+    element: gst::Element,
+}
+
+struct Scene {
+    element: gst::Element,
+}
+
+struct Sink {
+    element: gst::Element,
+}
+
+struct App<'a> {
+    sources: Vec<Sink>,
+    scenes: Vec<Scene>,
+    wrapped_sources: Vec<WrappedSource<'a>>,
+    sinks: Vec<Sink>,
 }
--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,46 +1,15 @@
 extern crate gstreamer as gst;
 use gst::prelude::*;
+extern crate dankcaster as dkc;
 
 fn main() {
-    gst::init().unwrap();
+    dkc::init().unwrap();
 
-    let elem = gst::ElementFactory::make("videotestsrc", "source")
-                       .expect("Could not create source element.");
-    let pad = elem.get_static_pad("src").unwrap();
-    println!("{:?}", pad);
-    let ghost_pad = gst::GhostPad::new("src", &pad).unwrap();
+    let app = dkc::makeApp(1280, 720).unwrap();
+    let dummy_source = app.make_source("dummy", "my dummy source");
+    let dummy_sink = app.make_sink("dummy", "my dummy sink");
+    let scene = app.make_scene("my scene");
+    let wrapped_dummy_source = scene.add_source(dummy_source);
 
-    let source = gst::ElementFactory::make("videotestsrc", "source")
-        .expect("Could not create source element.");
-    let sink = gst::ElementFactory::make("xvimagesink", "sink")
-        .expect("Could not create sink element.");
-    let pipeline = gst::Pipeline::new("my-pipeline");
-
-    pipeline.add_many(&[&source,&sink]).unwrap();
-    source.link(&sink).expect("Elements could not be linked.");
-    sink.set_property_from_str("sync", "false");
-
-    let ret = pipeline.set_state(gst::State::Playing);
-    assert!(ret.is_ok(), "Unable to set the pipeline to the playing state");
-
-    let bus = pipeline.get_bus().unwrap();
-    while let Some(msg) = bus.timed_pop(gst::CLOCK_TIME_NONE) {
-        use gst::MessageView;
-        match msg.view() {
-            MessageView::Error(err) => {
-                eprintln!(
-                    "Error received from element {:?}: {}",
-                    err.get_src().map(|s| s.get_path_string()),
-                    err.get_error()
-                );
-                eprintln!("Debugging information: {:?}", err.get_debug());
-                break;
-            }
-            MessageView::Eos(..) => break,
-            _ => (),
-        }
-    }
-
-    let ret = pipeline.set_state(gst::State::Null);
-    assert!(ret.is_ok(), "Unable to set the pipeline to the Null state.");
+    dkc::terminate();
 }
