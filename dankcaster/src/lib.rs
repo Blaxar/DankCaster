271864d20486819abedcfe5321e4575e5bc5fea1
Blaxar Waldarax <blaxar.waldarax@gmail.com>
2019-11-10 16:48:55 +0100

    Done with make_source and make_sink implementations, updated tests

--- dankcaster/src/lib.rs
+++ dankcaster/src/lib.rs
@@ -87,6 +87,8 @@ fn make_app(name: Option<&str>, width : u16, height: u16) -> Result<App, Error> 
                                  scenes: RefCell::new(vec![]),
                                  wrapped_sources: RefCell::new(vec![]),
                                  sinks: RefCell::new(vec![])});
+
+    app.gst_bin.add(&app.gst_scene);
     Ok(App { app })
 
 }
@@ -101,15 +103,53 @@ impl App {
             Some(element) => {
                 let id = self.app.sources.borrow_mut().len();
 
-                let mut source = Rc::new(
+                let source = Rc::new(
                     Source { app: self.app.clone(), element, id });
 
                 self.app.sources.borrow_mut().push(source.clone());
 
-                self.app.gst_bin.add(&source.element);
+                if self.app.gst_bin.add(&source.element).is_err() {
+                    return Err(Error {});
+                }
 
-                // TODO: link pads here
-                
+                let video_ret : std::result::Result<(), Error> = match source.element.get_static_pad("video_src") {
+                    Some(video_src_pad) => {
+                        match self.app.gst_scene.get_request_pad("video_sink_%u") {
+                            Some(video_sink_pad) => {
+                                match video_src_pad.link(&video_sink_pad) {
+                                    Ok(_success) => Ok(()),
+                                    Err(_error) => Err(Error {}),
+                                }
+                            },
+                            None => Err(Error {})
+                        }
+                    },
+                    None => Ok(()),
+                };
+
+                if video_ret.is_err() {
+                    return Err(Error {});
+                }
+
+                let audio_ret : std::result::Result<(), Error> = match source.element.get_static_pad("audio_src") {
+                    Some(audio_src_pad) => {
+                        match self.app.gst_scene.get_request_pad("audio_sink_%u") {
+                            Some(audio_sink_pad) => {
+                                match audio_src_pad.link(&audio_sink_pad) {
+                                    Ok(_success) => Ok(()),
+                                    Err(_error) => Err(Error {}),
+                                }
+                            },
+                            None => Err(Error {})
+                        }
+                    },
+                    None => Ok(()),
+                };
+
+                if audio_ret.is_err() {
+                    return Err(Error {});
+                }
+                            
                 Ok(Rc::downgrade(&source))
             },
             None => Err(Error {}),
@@ -131,9 +171,47 @@ impl App {
 
                 self.app.sinks.borrow_mut().push(sink.clone());
 
-                self.app.gst_bin.add(&sink.element);
+                if self.app.gst_bin.add(&sink.element).is_err() {
+                    return Err(Error {});
+                }
 
-                // TODO: link pads here
+                let video_ret : std::result::Result<(), Error> = match sink.element.get_static_pad("video_sink") {
+                    Some(video_sink_pad) => {
+                        match self.app.gst_scene.get_request_pad("video_src_%u") {
+                            Some(video_src_pad) => {
+                                match video_sink_pad.link(&video_src_pad) {
+                                    Ok(_success) => Ok(()),
+                                    Err(_error) => Err(Error {}),
+                                }
+                            },
+                            None => Err(Error {})
+                        }
+                    },
+                    None => Ok(()),
+                };
+
+                if video_ret.is_err() {
+                    return Err(Error {});
+                }
+
+                let audio_ret : std::result::Result<(), Error> = match sink.element.get_static_pad("audio_sink") {
+                    Some(audio_sink_pad) => {
+                        match self.app.gst_scene.get_request_pad("audio_src_%u") {
+                            Some(audio_src_pad) => {
+                                match audio_sink_pad.link(&audio_src_pad) {
+                                    Ok(_success) => Ok(()),
+                                    Err(_error) => Err(Error {}),
+                                }
+                            },
+                            None => Err(Error {})
+                        }
+                    },
+                    None => Ok(()),
+                };
+
+                if audio_ret.is_err() {
+                    return Err(Error {});
+                }
                 
                 Ok(Rc::downgrade(&sink))
             },
@@ -199,12 +277,30 @@ mod tests {
 
         let mut app = make_app(Some("test"), 1280, 720).expect("Could not make app.");
 
+        assert_eq!(app.app.sources.borrow_mut().len(), 0);
+
+        let source = app.make_source("dummy", None);
+
         assert!(
-            match app.make_source("dummy", None) {
+            match &source {
                 Ok(el) => true,
                 Err(err) => false
             }
         );
+
+        let gst_src = &source.unwrap().upgrade().unwrap().element; 
+
+        assert!(match gst_src.get_static_pad("video_src").unwrap().get_peer() {
+            Some(peer_pad) => peer_pad.get_name().eq("video_sink_0"),
+            None => false
+        });
+
+        assert!(match gst_src.get_static_pad("audio_src").unwrap().get_peer() {
+            Some(peer_pad) => peer_pad.get_name().eq("audio_sink_0"),
+            None => false
+        });
+
+        assert_eq!(app.app.sources.borrow_mut().len(), 1);
 
         assert!(
             match app.make_source("IdoNotExist", None) {
@@ -220,14 +316,32 @@ mod tests {
 
         set_up();
 
-        let mut app = make_app(Some("test"), 1280, 720).expect("Could not make app");
+        let mut app = make_app(Some("test"), 1280, 720).expect("Could not make app.");
+
+        assert_eq!(app.app.sinks.borrow_mut().len(), 0);
+
+        let sink = app.make_sink("dummy", None);
 
         assert!(
-            match app.make_sink("dummy", None) {
+            match &sink {
                 Ok(el) => true,
                 Err(err) => false
             }
         );
+
+        let gst_sink = &sink.unwrap().upgrade().unwrap().element; 
+
+        assert!(match gst_sink.get_static_pad("video_sink").unwrap().get_peer() {
+            Some(peer_pad) => peer_pad.get_name().eq("video_src_0"),
+            None => false
+        });
+
+        assert!(match gst_sink.get_static_pad("audio_sink").unwrap().get_peer() {
+            Some(peer_pad) => peer_pad.get_name().eq("audio_src_0"),
+            None => false
+        });
+
+        assert_eq!(app.app.sinks.borrow_mut().len(), 1);
 
         assert!(
             match app.make_sink("IdoNotExist", None) {
